2ea8b4fc7df78313cd1a8263614e23e32f4f12ce
Tim Huff <timhuff@gmail.com>
2018-03-30 06:01:18 -0500

    alright. looking good

--- server/api/models/Timecard.js
+++ server/api/models/Timecard.js
@@ -3,12 +3,13 @@ import APIModel from 'server/api/util/APIModel'
 import { QueryBuilder, Model } from 'objection'
 import { GraphQLString } from 'graphql'
 import ExpectedError from 'server/errors/ExpectedError'
+import moment from 'moment-timezone'
 
 export default class Timecard extends withDeletedAt(APIModel) {
   static knexCreateTable = `
     table.uuid('id').primary().defaultTo(knex.raw("uuid_generate_v4()"))
     table.timestamp('deletedAt').index()
-    table.uuid('employeeId').notNullable()
+    table.uuid('employeeId')
     table.date('date')
     table.timestamp('clockedInAt')
     table.timestamp('clockedOutAt')
@@ -39,6 +40,11 @@ export default class Timecard extends withDeletedAt(APIModel) {
     return class extends QueryBuilder {
       _contextFilter() {
         // this.whereRaw('FALSE')
+      }
+      _mine() {
+        const { session } = this.context()
+        if (!session) return this.whereRaw('FALSE')
+        this.where({ employeeId: session.account.employee.id }).whereNull('clockedOutAt')
       }
     }
   }
@@ -90,19 +96,20 @@ export default class Timecard extends withDeletedAt(APIModel) {
         },
         resolve: async (root, { vehicleExternalId }, { session }) => {
           if (!session) throw new ExpectedError('Unauthorized Access')
-          const Vehicle = require('./Vehicle').default
-          const vehicle = await Vehicle.query()
-          .where({ externalId: vehicleExternalId })
+          const existingTimecard = await Timecard.query()
+          .whereNull('clockedOutAt')
+          .where({ employeeId: session.account.employee.id })
           .first()
-
-          if (!vehicle) throw new ExpectedError('Unable to find a vehicle with that identifier')
+          if (existingTimecard) throw new ExpectedError('You already have a timecard. Clock out to create a new one.')
           const timecard = await Timecard.query()
           .insert({
-            employeeId: session.account.employee.id,
-            vehicleId: vehicle.id,
+            clockedInAt: moment().format(),
           })
           .returning('*')
-          console.log(timecard)
+
+          await timecard.$relatedQuery('employee').relate(session.account.employee)
+
+          await timecard.$loadRelated('employee')
           return timecard
         },
       },
