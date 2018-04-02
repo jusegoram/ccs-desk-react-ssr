c440decef8aeac503a7c1d9b568ea7131176d237
Sebastian Gomez <jseb16@gmail.com>
2018-04-01 20:00:03 -0500

    production knexfile config

--- knexfile.js
+++ knexfile.js
@@ -20,4 +20,26 @@ module.exports = {
       directory: './server/db/seeds',
     },
   },
+  production: {
+    client: 'postgresql',
+    connection: {
+      host: 'endeavorfleet.coevycwg7sov.us-east-1.rds.amazonaws.com',
+      user: 'master',
+      password: process.env.PRODUCTION_PASSWORD,
+      database: 'endeavor',
+      multipleStatements: true,
+      charset: 'utf8',
+    },
+    pool: {
+      min: 4,
+      max: 1024,
+    },
+    migrations: {
+      tableName: 'knex_migrations',
+      directory: './server/db/migrations',
+    },
+    seeds: {
+      directory: './server/db/seeds',
+    },
+  },
 }
