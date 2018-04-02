493ae45c203f71ae6bef6cda346a98be2ce990b7
Sebastian Gomez <jseb16@gmail.com>
2018-04-02 02:33:02 -0500

    clean up

--- pages/sign-in.js
+++ pages/sign-in.js
@@ -20,7 +20,6 @@ class SignIn extends React.Component {
                   {createSession => (
                     <Login
                       onSubmit={variables => {
-                        console.log(variables)
                         createSession({ variables })
                       }}
                     />
