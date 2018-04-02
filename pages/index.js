e6f19a888aa04c19467bd18fe9faacf2bb340d38
Sebastian Gomez <jseb16@gmail.com>
2018-04-02 05:27:58 -0500

    added download button

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactTable from 'react-table'
 import { Query } from 'react-apollo'
 import moment from 'moment-timezone'
+import { Card, CardHeader, CardBody } from 'reactstrap'
 
 import withApolloProvider from 'app/apollo/withApolloProvider'
 import data from 'app/apollo/data'
@@ -41,20 +42,32 @@ class Timecards extends React.Component {
           <Query {...data.Timecard.QUERY} fetchPolicy="cache-and-network" pollInterval={5000}>
             {({ loading, data }) => {
               return (
-                <ReactTable
-                  style={{ backgroundColor: 'white', height: 'calc(100vh - 100px)' }}
-                  filterable
-                  className="-striped -highlight"
-                  loading={!data.timecards && loading}
-                  data={data && data.timecards}
-                  defaultPageSize={20}
-                  columns={columns}
-                  defaultFilterMethod={(filter, row) =>
-                    String(row[filter.id])
-                    .toLowerCase()
-                    .indexOf(String(filter.value).toLowerCase()) !== -1
-                  }
-                />
+                <Card>
+                  <CardHeader>
+                    <i className="icon-menu" /> Timecards
+                    <div className="card-actions">
+                      <a href="#">
+                        <i className="fa fa-download fa-lg" />
+                      </a>
+                    </div>
+                  </CardHeader>
+                  <CardBody>
+                    <ReactTable
+                      style={{ backgroundColor: 'white', height: 'calc(100vh - 100px)' }}
+                      filterable
+                      className="-striped -highlight"
+                      loading={!data.timecards && loading}
+                      data={data && data.timecards}
+                      defaultPageSize={20}
+                      columns={columns}
+                      defaultFilterMethod={(filter, row) =>
+                        String(row[filter.id])
+                        .toLowerCase()
+                        .indexOf(String(filter.value).toLowerCase()) !== -1
+                      }
+                    />
+                  </CardBody>
+                </Card>
               )
             }}
           </Query>
