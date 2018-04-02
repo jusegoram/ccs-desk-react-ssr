18393903764a5ab3ce6f0963dfac0869595ec986
Sebastian Gomez <jseb16@gmail.com>
2018-04-02 02:52:49 -0500

    fixed

--- app/ui/Page.js
+++ app/ui/Page.js
@@ -14,44 +14,42 @@ import { SessionProvider, LocationProvider } from 'app/util/providers'
 import stylesheet from 'app/styles/index.scss'
 import 'react-select/dist/react-select.css'
 
-const Page = withApolloProvider(
-  ({ title, redirectAuthedUserTo, redirectUnauthedUserTo = '/sign-in', location, children }) =>
-    React.Children.only(
-      <div>
-        <Head>
-          <title>Endeavor Fleet{title && ` | ${title}`}</title>
-          <link rel="shortcut icon" type="image/x-icon" href="/static/favicon.ico" />
-          <link rel="stylesheet" href="/static/index.fonts.css" />
-          <link rel="stylesheet" href="/static/index.styles.css" />
-          <style dangerouslySetInnerHTML={{ __html: stylesheet }} />
-        </Head>
-        <LocationProvider location={location}>
-          <NetworkProgressBar />
-          {!redirectAuthedUserTo && !redirectUnauthedUserTo ? (
-            children
-          ) : (
-            <Query {...data.Session.GET} fetchPolicy="cache-and-network">
-              {result => {
-                const { loading, data } = result
-                console.log('data', data)
-                if (redirectUnauthedUserTo && !loading && (!data || !data.session)) {
-                  Router.replace(redirectUnauthedUserTo)
-                  return null
-                }
-                if (redirectAuthedUserTo && !loading && data && data.session) {
-                  Router.replace(redirectAuthedUserTo)
-                  return null
-                }
-                if (!data && loading) {
-                  return <div>Loading...</div>
-                }
-                return <SessionProvider session={data.session}>{children}</SessionProvider>
-              }}
-            </Query>
-          )}
-        </LocationProvider>
-      </div>
-    )
+const Page = withApolloProvider(({ title, authed = true, location, children }) =>
+  React.Children.only(
+    <div>
+      <Head>
+        <title>Endeavor Fleet{title && ` | ${title}`}</title>
+        <link rel="shortcut icon" type="image/x-icon" href="/static/favicon.ico" />
+        <link rel="stylesheet" href="/static/index.fonts.css" />
+        <link rel="stylesheet" href="/static/index.styles.css" />
+        <style dangerouslySetInnerHTML={{ __html: stylesheet }} />
+      </Head>
+      <LocationProvider location={location}>
+        <NetworkProgressBar />
+        {authed === null ? (
+          children
+        ) : (
+          <Query {...data.Session.GET} fetchPolicy="cache-and-network">
+            {result => {
+              const { loading, data } = result
+              if (authed && !loading && (!data || !data.session)) {
+                Router.replace('/sign-in')
+                return null
+              }
+              if (!authed && !loading && data && data.session) {
+                Router.replace('/')
+                return children
+              }
+              if (!data && loading) {
+                return <div>Loading...</div>
+              }
+              return <SessionProvider session={data.session}>{children}</SessionProvider>
+            }}
+          </Query>
+        )}
+      </LocationProvider>
+    </div>
+  )
 )
 
 export default Page
