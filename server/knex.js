13578a15abe1b4b0eae4f64fe077a272bb549c58
Tim Huff <timhuff@gmail.com>
2018-04-01 20:28:32 -0500

    removed debug

--- server/knex.js
+++ server/knex.js
@@ -1,7 +1,6 @@
 import Knex from 'knex'
 import knexfile from 'server/../knexfile'
 
-console.log(knexfile[process.env.NODE_ENV])
 export default Knex({
   debug: process.env.NODE_ENV === 'development',
   ...knexfile[process.env.NODE_ENV],
