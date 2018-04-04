80b37371736889b3a24cf59b615fe7f323a8d221
Sebastian Gomez <jseb16@gmail.com>
2018-04-04 01:22:58 -0500

    links should typically be shallow

--- app/ui/Layout/HeaderDropdown.js
+++ app/ui/Layout/HeaderDropdown.js
@@ -165,7 +165,7 @@ class HeaderDropdown extends Component {
               <i className="fa icon-book-open" /> View Invites
             </DropdownItem>
           </Link> */}
-          <Link href="/root/mimic">
+          <Link shallow href="/root/mimic">
             <DropdownItem onClick={() => this.logout(p(props, 'session.id'))}>
               <i className="fa fa-user-circle" /> Mimic User
             </DropdownItem>
