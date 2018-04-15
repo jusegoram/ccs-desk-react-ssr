5c2f1bac644121f6cdaba07eec4fa00e7ddb5ab1
Sebastian Gomez <jseb16@gmail.com>
2018-04-15 16:07:28 -0500

    tech data page now displays info about last 10

--- app/apollo/data/index.js
+++ app/apollo/data/index.js
@@ -1,9 +1,10 @@
 /* eslint-disable import/prefer-default-export */
 import Account from './Account'
+import DataImport from './DataImport'
 import Employee from './Employee'
 import Session from './Session'
 import Timecard from './Timecard'
 import VehicleClaim from './VehicleClaim'
 import WorkGroup from './WorkGroup'
 
-export default { Account, Employee, Session, Timecard, VehicleClaim, WorkGroup }
+export default { Account, DataImport, Employee, Session, Timecard, VehicleClaim, WorkGroup }
