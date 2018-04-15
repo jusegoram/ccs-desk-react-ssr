79298e182fdc62b4f86405bd3d87b8b9f1bbf571
Sebastian Gomez <jseb16@gmail.com>
2018-04-15 13:51:15 -0500

    new nav elements

--- app/ui/Layout/_nav.js
+++ app/ui/Layout/_nav.js
@@ -1,18 +1,36 @@
 export default {
   items: [
     {
+      title: true,
+      name: 'Organization',
+      wrapper: {
+        // optional wrapper object
+        element: 'span', // required valid HTML5 element tag
+        attributes: {}, // optional valid JS object with JS API naming ex: { className: 'my-class' }
+      },
+      class: 'text-center', // optional class names space delimited list for title item ex: 'text-center'
+    },
+    {
       name: 'Techs',
       url: '/techs',
       icon: 'icon-people',
     },
+    {
+      title: true,
+      name: 'Data',
+      wrapper: {
+        element: 'span',
+      },
+      class: 'text-center',
+    },
+    {
+      name: 'Tech Data',
+      url: '/data/techs',
+      icon: 'fa fa-users',
+    },
     // {
-    //   name: 'Timecards',
-    //   url: '/',
-    //   icon: 'icon-clock',
-    // },
-    // {
-    //   name: 'Vehicle Claims',
-    //   url: '/vehicle-claims',
+    //   name: 'Work Order Data',
+    //   url: '/data/work-orders',
     //   icon: 'fa fa-car',
     // },
   ],
