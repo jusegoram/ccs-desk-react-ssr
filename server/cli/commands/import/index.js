f8960df3f3948c9506481927c882052328666d7a
Tim Huff <timhuff@gmail.com>
2018-04-16 08:02:39 -0500

    Revert "fixed time frame"
    This reverts commit 84e27f4ddb3572cfc227d6b1c22deda62c3dec6e.

--- server/cli/commands/import/index.js
+++ server/cli/commands/import/index.js
@@ -87,28 +87,28 @@ module.exports = async ({ companyName, dataSourceName, reportName }) => {
   .insert({ dataSourceId: dataSource.id, reportName })
   .returning('*')
   try {
-    // process.stdin.resume() //so the program will not close instantly
+    process.stdin.resume() //so the program will not close instantly
 
-    // const exitHandler = (options, err) => {
-    //   if (options.cleanup) {
-    //     dataImport.$query().patch({ status: 'Aborted' })
-    //   }
-    //   if (err) console.log(err.stack)
-    //   if (options.exit) process.exit()
-    // }
+    const exitHandler = (options, err) => {
+      if (options.cleanup) {
+        dataImport.$query().patch({ status: 'Aborted' })
+      }
+      if (err) console.log(err.stack)
+      if (options.exit) process.exit()
+    }
 
-    // //do something when app is closing
-    // process.on('exit', exitHandler.bind(null, { cleanup: true }))
+    //do something when app is closing
+    process.on('exit', exitHandler.bind(null, { cleanup: true }))
 
-    // //catches ctrl+c event
-    // process.on('SIGINT', exitHandler.bind(null, { exit: true }))
+    //catches ctrl+c event
+    process.on('SIGINT', exitHandler.bind(null, { exit: true }))
 
-    // // catches "kill pid" (for example: nodemon restart)
-    // process.on('SIGUSR1', exitHandler.bind(null, { exit: true }))
-    // process.on('SIGUSR2', exitHandler.bind(null, { exit: true }))
+    // catches "kill pid" (for example: nodemon restart)
+    process.on('SIGUSR1', exitHandler.bind(null, { exit: true }))
+    process.on('SIGUSR2', exitHandler.bind(null, { exit: true }))
 
-    // //catches uncaught exceptions
-    // process.on('uncaughtException', exitHandler.bind(null, { exit: true }))
+    //catches uncaught exceptions
+    process.on('uncaughtException', exitHandler.bind(null, { exit: true }))
 
     const credentials = analyticsCredentials[companyName]
     await dataImport.$query().patch({ status: 'Downloading' })
