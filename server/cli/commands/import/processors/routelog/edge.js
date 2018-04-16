971b149515cfbe66915b6e62738351836f324bea
Sebastian Gomez <jseb16@gmail.com>
2018-04-16 00:30:54 -0500

    got routelog processors sorted out

--- server/cli/commands/import/processors/routelog/edge.js
+++ server/cli/commands/import/processors/routelog/edge.js
@@ -6,18 +6,7 @@ import * as rawModels from 'server/api/models'
 import moment from 'moment-timezone'
 import { streamToArray } from 'server/util'
 import Timer from 'server/util/Timer'
-
-const serviceW2Company = {
-  'Goodman Analytics': 'Goodman',
-  'DirectSat Analytics': 'DirectSat',
-}
-const getDateString = timeString => {
-  if (!timeString) return null
-  const badDateString = timeString.split(' ')[0]
-  const date = moment(badDateString, 'M/D/YY')
-  if (!date.isValid()) return null
-  return date.format('YYYY-MM-DD')
-}
+import handleStandardRows from 'server/cli/commands/import/processors/routelog/handleStandardRows'
 
 const convertRowToStandardForm = ({ row, w2Company, employee }) => {
   const getWorkGroup = type => (employee && _.find(employee.workGroups, { type })) || {}
@@ -90,19 +79,16 @@ const convertRowToStandardForm = ({ row, w2Company, employee }) => {
   '# of Activities': '1' }
 */
 
-export default async ({ csvObjStream, dataSource }) => {
+export default async ({ csvObjStream, w2Company, dataSource }) => {
   const timer = new Timer()
   timer.start('Total')
   timer.start('Initialization')
   await transaction(..._.values(rawModels), async (...modelsArray) => {
     const models = _.keyBy(modelsArray, 'name')
-    const { WorkOrder, WorkGroup, Company, Employee } = models
-    const knex = WorkOrder.knex()
-    const dataSourceId = dataSource.id
-    const workGroupCache = {}
+    const { WorkGroup, Employee } = models
 
     timer.split('SR Data Load')
-    const w2CompanyName = serviceW2Company[dataSource.service]
+    const w2CompanyName = w2Company.name
     const srData = _.keyBy(
       await WorkGroup.knex()
       .select('Service Region', 'Office', 'DMA', 'Division')
@@ -111,11 +97,8 @@ export default async ({ csvObjStream, dataSource }) => {
       'Service Region'
     )
 
-    timer.split('Ensure Company')
-    const w2Company = await Company.query().ensure(w2CompanyName)
-
     // timer.split('Stream to Array')
-    const workOrderDatas = await streamToArray(csvObjStream, data => {
+    const datas = await streamToArray(csvObjStream, data => {
       const serviceRegion = data['Service Region']
       const groups = srData[serviceRegion]
       if (groups) {
@@ -129,123 +112,14 @@ export default async ({ csvObjStream, dataSource }) => {
       return data
     })
 
-    timer.split('Load Existing')
-    const dbWorkOrders = _.keyBy(
-      await WorkOrder.query()
-      .eager('workGroups')
-      .where({ dataSourceId })
-      .where(
-        'date',
-        '>=',
-        moment
-        .tz('America/Los_Angeles')
-        .add(-1, 'day')
-        .format('YYYY-MM-DD')
-      ),
-      'externalId'
-    )
-
-    await Promise.mapSeries(workOrderDatas, async data => {
-      timer.split('Fetch Employee')
-      const employee =
-        data.assignedTechId &&
-        (await Employee.query()
-        .eager('[company, workGroups]')
-        .first()
-        .where({ alternateExternalId: data.assignedTechId }))
-
-      data.row = convertRowToStandardForm({ row: data, w2Company, employee })
-
-      timer.split('Work Order Upsert')
-      const dbWorkOrder = dbWorkOrders[data['Activity ID']]
-      let workOrder = dbWorkOrder
-      if (!workOrder || !_.isEqual(workOrder.data, data)) {
-        workOrder = await WorkOrder.query()
-        .eager('workGroups')
-        .upsert({
-          query: { dataSourceId, externalId: data['Activity ID'] },
-          update: {
-            date: getDateString(data['Due Date']),
-            type: data['Order Sub Type'],
-            status: data['Status'],
-            row: data.row,
-          },
-        })
-      }
-
-      timer.split('Work Group Datas')
-      const workGroupDatas = [
-        {
-          companyId: w2Company.id,
-          type: 'Company',
-          externalId: w2Company.name,
-          name: w2Company.name,
-        },
-        ...(!employee
-          ? []
-          : [
-            ...employee.workGroups.map(workGroup => ({
-              companyId: workGroup.companyId,
-              type: workGroup.type,
-              externalId: workGroup.externalId,
-              name: workGroup.name,
-            })),
-          ]),
-        ...(!!data.SR && [
-          {
-            companyId: w2Company.id,
-            type: 'Service Region',
-            externalId: data.SR,
-            name: data.SR,
-          },
-          {
-            companyId: w2Company.id,
-            type: 'Office',
-            externalId: data.Office,
-            name: data.Office,
-          },
-          {
-            companyId: w2Company.id,
-            type: 'DMA',
-            externalId: data.DMA,
-            name: data.DMA,
-          },
-          {
-            companyId: w2Company.id,
-            type: 'Division',
-            externalId: data.Division,
-            name: data.Division,
-          },
-        ]),
-      ]
-
-      timer.split('Work Groups _.differenceWith')
-      const workGroupPrimaryKey = ['companyId', 'type', 'externalId']
-      const hasSamePrimaryKey = (a, b) => _.isEqual(_.pick(a, workGroupPrimaryKey), _.pick(b, workGroupPrimaryKey))
-      const newWorkGroupDatas = _.differenceWith(workGroupDatas, workOrder.workGroups, hasSamePrimaryKey)
-      const obsoleteWorkGroups = _.differenceWith(workOrder.workGroups, workGroupDatas, hasSamePrimaryKey)
-
-      timer.split('Ensure New Work Groups')
-      const newWorkGroups = await Promise.map(newWorkGroupDatas, workGroupData =>
-        WorkGroup.query().ensure(workGroupData, workGroupCache)
-      )
-
-      timer.split('Insert New Work Group Relations')
-      await Promise.mapSeries(_.uniqBy(newWorkGroups, 'id'), workGroup =>
-        knex('workGroupWorkOrders').insert({
-          workOrderId: workOrder.id,
-          workGroupId: workGroup.id,
-        })
-      )
-
-      timer.split('Delete Old Work Group Relations')
-      if (obsoleteWorkGroups.length) {
-        await knex('workGroupWorkOrders')
-        .where({ workOrderId: workOrder.id })
-        .whereIn('workGroupId', _.map(obsoleteWorkGroups, 'id'))
-        .delete()
-      }
+    const rows = await Promise.mapSeries(datas, async data => {
+      const employee = await Employee.query()
+      .eager('[company, workGroups]')
+      .findOne({ dataSourceId: dataSource.id, alternateExternalId: data['Tech ID'] })
+      return convertRowToStandardForm({ row: data, w2Company, employee })
     })
+
+    await handleStandardRows({ rows, timer, models, dataSource, w2Company })
   })
   timer.stop('Total')
   console.log(timer.toString()) // eslint-disable-line no-console
