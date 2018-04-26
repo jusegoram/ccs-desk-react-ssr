73cb778d119ef356346019c7d4c7b4264c583d97
Tim Huff <timhuff@gmail.com>
2018-04-25 22:38:10 -0500

    reschedules marked

--- server/index.js
+++ server/index.js
@@ -193,6 +193,15 @@ export default async app => {
         .eager('appointments')
         .whereIn('id', workOrderIdsScheduledTodayAtSomePointToday)
         .map(async workOrder => {
+          if (!workOrder.appointments || workOrder.appointments.length < 2) {
+            return workOrder
+          }
+          const sortedAppointments = _.sortBy(workOrder.appointments, 'createdAt')
+          const currentAppointment = sortedAppointments.slice(-1)[0]
+          if (currentAppointment.date !== moment().format('YYYY-MM-DD')) workOrder.row['Status'] = 'Rescheduled'
+          return workOrder
+        })
+        .map(async workOrder => {
           workOrder.row = _.mapValues(workOrder.row, val => (val === true ? 'TRUE' : val === false ? 'FALSE' : val))
           return workOrder
         })
