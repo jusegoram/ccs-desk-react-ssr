f5da8ecbb7b11b9dd5a6e37e9c1f0aa6266267e3
Tim Huff <timhuff@gmail.com>
2018-04-27 14:18:45 -0500

    updated config and changed root account name

--- server/config.js
+++ server/config.js
@@ -1,4 +1,4 @@
 export default {
-  host: 'https://beta.ccsdesk.com',
+  host: process.env.NODE_ENV === 'development' ? 'http://localhost:3000' : 'https://beta.ccsdesk.com',
   emailSender: 'CCS Desk<info@ccsdesk.com>',
 }
