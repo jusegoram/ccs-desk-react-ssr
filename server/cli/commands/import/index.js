b23d75abae4ccc670abbb777a08369286edbce12
Tim Huff <timhuff@gmail.com>
2018-05-04 10:13:01 -0500

    dont run reports before 4 am

--- server/cli/commands/import/index.js
+++ server/cli/commands/import/index.js
@@ -86,6 +86,7 @@ const dataSourceNames = {
 
 // const screenshotsDirectory = path.resolve(__dirname, 'screenshots')
 module.exports = async ({ companyName, dataSourceName, reportName }) => {
+  if (moment.tz('America/Chicago').hour() < 4) return
   const w2Company = await Company.query().findOne({ name: companyName })
   const dataSource = await w2Company
   .$relatedQuery('dataSources')
