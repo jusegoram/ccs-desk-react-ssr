8a2fcaba92509c5c2b9205d80cf57f9a40dfa90f
Sebastian Gomez <jseb16@gmail.com>
2018-05-05 09:57:11 -0500

    added new aliases

--- server/cli/commands/import/processors/sanitizeCompanyName.js
+++ server/cli/commands/import/processors/sanitizeCompanyName.js
@@ -1,6 +1,8 @@
 const aliases = {
   'DW Direct INC': 'DW DIRECT',
   Empath: 'EMPATH',
+  'Empath LLC': 'EMPATH',
+  'SUNRISE GROUPS LLC': 'SUNRISE GROUP',
 }
 
 export default name => aliases[name] || name
