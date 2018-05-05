b77d8ee3ad205af87e1cc89d3dc7c9318fae56ca
Sebastian Gomez <jseb16@gmail.com>
2018-05-05 16:05:48 -0500

    fixed sclosed

---
+++ pages/sdcr.js
@@ -0,0 +1,62 @@
+import React from 'react'
+import ReactTable from 'react-table'
+import { Query } from 'react-apollo'
+import moment from 'moment-timezone'
+import { Card, CardHeader, CardBody, Button } from 'reactstrap'
+import alert from 'sweetalert'
+
+import asNextJSPage from 'app/util/asNextJSPage'
+import data from 'app/apollo/data'
+
+import Layout from 'app/ui/Layout'
+
+class SDCR extends React.Component {
+  render() {
+    return (
+      <Layout>
+        <Query {...data.WorkGroupMetric.QUERY} fetchPolicy="cache-and-network" pollInterval={5000}>
+          {({ loading, data }) => {
+            return (
+              <Card>
+                <CardHeader style={{ position: 'relative' }}>
+                  {/*relative because card-actions is absolute*/}
+                  <i className="icon-menu" /> Timecards
+                  <Button
+                    className="card-actions mt-0 h-100"
+                    color="primary"
+                    onClick={() => {
+                      window.open('https://endeavorfleet.com/download/timecards')
+                      alert(
+                        "The download should be starting. If it hasn't, verify that your popup blocker isn't preventing it from opening."
+                      )
+                    }}
+                  >
+                    <i className="fa fa-download fa-lg" />
+                  </Button>
+                </CardHeader>
+                <CardBody className="p-0">
+                  <ReactTable
+                    style={{ backgroundColor: 'white', height: 'calc(100vh - 146px)' }}
+                    filterable
+                    className="-striped -highlight"
+                    loading={!data.timecards && loading}
+                    data={data && data.timecards}
+                    defaultPageSize={20}
+                    columns={columns}
+                    defaultFilterMethod={(filter, row) =>
+                      String(row[filter.id])
+                      .toLowerCase()
+                      .indexOf(String(filter.value).toLowerCase()) !== -1
+                    }
+                  />
+                </CardBody>
+              </Card>
+            )
+          }}
+        </Query>
+      </Layout>
+    )
+  }
+}
+
+export default asNextJSPage(SDCR)
