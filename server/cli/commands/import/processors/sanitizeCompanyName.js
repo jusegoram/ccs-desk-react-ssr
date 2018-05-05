e14d97c9bd7e8d2232d38a53d56d018348f328f9
Tim Huff <timhuff@gmail.com>
2018-05-05 10:01:01 -0500

    ignore sunrise for now

--- server/cli/commands/import/processors/sanitizeCompanyName.js
+++ server/cli/commands/import/processors/sanitizeCompanyName.js
@@ -2,7 +2,6 @@ const aliases = {
   'DW Direct INC': 'DW DIRECT',
   Empath: 'EMPATH',
   'Empath LLC': 'EMPATH',
-  'SUNRISE GROUPS LLC': 'SUNRISE GROUP',
 }
 
 export default name => aliases[name] || name
