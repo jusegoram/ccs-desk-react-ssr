ea91eef829fed74e804e1562411b8d454c4388f9
Sebastian Gomez <jseb16@gmail.com>
2018-05-07 01:03:12 -0500

    Good enough for now

--- server/api/restRouter/index.js
+++ server/api/restRouter/index.js
@@ -2,10 +2,12 @@ import express from 'express'
 
 import imageRouter from './image'
 import sdcrRouter from './sdcr'
+import workGroupRouter from './workGroup'
 
 const router = express.Router()
 
 router.use('/image', imageRouter)
 router.use('/sdcr', sdcrRouter)
+router.use('/workGroup', workGroupRouter)
 
 export default router
