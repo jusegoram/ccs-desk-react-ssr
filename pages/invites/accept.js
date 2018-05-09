94a3e8d1d8d2294b29e6fbf5b99fbe410a8940e7
Sebastian Gomez <jseb16@gmail.com>
2018-05-08 23:15:03 -0500

    invites

---
+++ pages/invites/accept.js
@@ -0,0 +1,77 @@
+import React from 'react'
+
+import { Col, Container, Row } from 'reactstrap'
+import { compose, graphql } from 'react-apollo'
+import gql from 'graphql-tag'
+import Page from 'app/ui/Page'
+import AcceptInviteForm from 'app/ui/Form/AcceptInvite'
+
+import wrappedByApolloProvider from 'app/apolloUtils/wrappedByApolloProvider'
+import { POLL_SESSION } from 'app/data/queries/session'
+import alert from 'sweetalert'
+import redirect from 'app/util/redirect'
+import withEnvVars from 'app/util/withEnvVars'
+import { SessionProvider } from 'app/data/providers/session'
+import { LocationProvider } from 'app/util/providers/location'
+import { RESET_PASSWORD } from 'app/data/queries/user'
+import Layout from 'app/ui/Layout'
+
+class AcceptInvite extends React.Component {
+  static async getInitialProps({ req }) {
+    return {
+      inviteToken: req && req.inviteToken,
+    }
+  }
+  constructor(props) {
+    super(props)
+    if (process.browser) {
+      this.inviteToken = this.inviteToken || props.inviteToken
+      if (!props.inviteToken) {
+        redirect('/')
+      } else if (props.url.asPath !== '/invites/accept') {
+        props.url.replace('/invites/accept')
+      }
+    }
+    this.state = {}
+    this.acceptInvite = this.acceptInvite.bind(this)
+  }
+
+  async acceptInvite(fields) {
+    const { password } = fields
+    const token = this.inviteToken
+    const variables = { password, token }
+    await this.props.acceptInvite({ variables })
+    await alert('Success', 'You have successfully registered an account. Sign in to continue.')
+    redirect('/sign-in')
+  }
+
+  render() {
+    const { location, session, ...props } = this.props
+    return (
+      <Page title="Accept Invite" {...props}>
+        <div className="app flex-row align-items-center">
+          <Container>
+            <Row className="justify-content-center">
+              <Col md="8" lg="5">
+                <AcceptInviteForm onSubmit={this.acceptInvite} />
+              </Col>
+            </Row>
+          </Container>
+        </div>
+      </Page>
+    )
+  }
+}
+
+const ACCEPT_INVITE = graphql(
+  gql`
+    mutation Invite_accept($token: String!, $password: String!) {
+      Invite_accept(token: $token, password: $password) {
+        id
+      }
+    }
+  `,
+  { name: 'acceptInvite' }
+)
+
+export default compose(wrappedByApolloProvider, ACCEPT_INVITE)(AcceptInvite)
---
+++ pages/invites/index.js
@@ -0,0 +1,66 @@
+import React from 'react'
+
+import wrappedByApolloProvider from 'app/apolloUtils/wrappedByApolloProvider'
+import MainApp from 'app/ui/MainApp'
+import ReactTable from 'react-table'
+import gql from 'graphql-tag'
+import { Query } from 'react-apollo'
+import Link from 'next/link'
+import _ from 'lodash'
+
+class Index extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = { limit: 20, offset: 0 }
+  }
+  render() {
+    const columns = [
+      { Header: 'Recipient', accessor: 'recipient.owner.name' },
+      { Header: 'Email', accessor: 'recipient.email' },
+      { Header: 'Status', accessor: 'status' },
+    ]
+    return (
+      <MainApp location={this.props.location}>
+        <Query query={INVITES} fetchPolicy="network-only">
+          {({ loading, error, data }) => {
+            return (
+              <ReactTable
+                style={{ backgroundColor: 'white', height: 'calc(100vh - 100px)' }}
+                filterable
+                className="-striped -highlight"
+                loading={loading}
+                data={data && data.invites}
+                defaultPageSize={20}
+                columns={columns}
+                defaultFilterMethod={(filter, row) =>
+                  String(row[filter.id])
+                  .toLowerCase()
+                  .indexOf(String(filter.value).toLowerCase()) !== -1
+                }
+              />
+            )
+          }}
+        </Query>
+      </MainApp>
+    )
+  }
+}
+
+const INVITES = gql`
+  query invites {
+    invites {
+      id
+      status
+      recipient {
+        id
+        email
+        owner {
+          id
+          name
+        }
+      }
+    }
+  }
+`
+
+export default wrappedByApolloProvider(Index)
