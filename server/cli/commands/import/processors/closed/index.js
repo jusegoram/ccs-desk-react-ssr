9009e18b34b3ca1441e87cae88ac74b53cabcdda
Sebastian Gomez <jseb16@gmail.com>
2018-05-16 06:53:09 -0500

    fixed up sclosed

--- server/cli/commands/import/processors/closed/index.js
+++ server/cli/commands/import/processors/closed/index.js
@@ -1,145 +1,144 @@
 import _ from 'lodash'
-import { transaction } from 'objection'
+import { transaction, raw } from 'objection'
 import * as rawModels from 'server/api/models'
 import { streamToArray } from 'server/util'
 import Timer from 'server/util/Timer'
 import Promise from 'bluebird'
 import moment from 'moment-timezone'
-import Company from 'server/api/models/Company'
 import WorkGroup from 'server/api/models/WorkGroup'
+import Company from 'server/api/models/Company'
+import sanitizeCompanyName from 'server/cli/commands/import/processors/sanitizeCompanyName'
 
-export default async ({ csvObjStream }) => {
+class ExpectedError extends Error {}
+
+export default async ({ csvObjStream, w2Company }) => {
   const timer = new Timer()
   timer.start('Total')
   timer.start('Initialization')
   await transaction(..._.values(rawModels), async (...modelsArray) => {
     const models = _.keyBy(modelsArray, 'name')
-    const { Tech, WorkOrder, SdcrDataPoint } = models
+    const { Tech, WorkOrder, SdcrDataPoint, Appointment } = models
+    const knex = Appointment.knex()
+
+    const srData = _.keyBy(
+      await knex('directv_sr_data').select('Service Region', 'Office', 'DMA', 'Division'),
+      'Service Region'
+    )
 
     const rows = await streamToArray(csvObjStream)
     let invalidRowsDetected = []
-    const sdcrData = []
     console.log(`${rows.length} rows to process`)
     let index = 0
-    await Promise.mapSeries(rows, async row => {
-      index++
-      if (!(index % 1000)) console.log(index / 1000)
-      let tech = null
-      const workOrder = await WorkOrder.query().findOne({ externalId: row['Activity ID'] })
-      if (row['Activity Status (Snapshot)'] === 'Closed' || row['Activity Status (Snapshot)'] === 'Pending Closed') {
-        tech = await Tech.query().findOne({ externalId: row['Tech ID'] })
-      } else {
-        let appointment =
-          workOrder &&
-          (await workOrder
-          .$relatedQuery('appointments')
-          .where(
-            'createdAt',
-            '<=',
-            moment(row['BGO Snapshot Date'], 'YYYY-MM-DD')
+    await Promise.map(
+      rows,
+      async row => {
+        index++
+        if (!(index % 1000)) console.log(index / 1000)
+        try {
+          const externalId = row['Activity ID']
+          if (row['Subcontractor Company Name'] === 'UNKNOWN') delete row['Subcontractor Company Name']
+          const subcontractorName = sanitizeCompanyName(row['Subcontractor Company Name'])
+          const subcontractor = await Company.query().findOne({ name: subcontractorName })
+
+          const tech = await (async () => {
+            if (
+              row['Activity Status (Snapshot)'] === 'Closed' ||
+              row['Activity Status (Snapshot)'] === 'Pending Closed'
+            ) {
+              const tech = await Tech.query().findOne({ externalId: row['Tech ID'] })
+              if (!tech) throw new ExpectedError(`Unable to find tech with tech ID ${row['Tech ID']}`)
+              return tech
+            }
+            const endOfBgoSnapshotDate = moment(row['BGO Snapshot Date'], 'YYYY-MM-DD')
             .endOf('day')
             .format()
-          )
-          .orderBy('createdAt', 'desc')
-          .first())
-        if (!appointment || !appointment.row) {
-          invalidRowsDetected.push({ reason: 'work order or appointment not found', row })
-          return
-        }
-        const techId = appointment && appointment.row['Tech ID']
-        tech = techId && (await Tech.query().findOne({ externalId: techId }))
-        if (!tech) {
-          tech = await Tech.query().findOne({ externalId: row['Tech ID'] })
-        }
-      }
+            const appointment = await Appointment.query()
+            .eager('assignedTech')
+            .findOne(raw('lifespan @> ? and "externalId" = ?', [endOfBgoSnapshotDate, externalId]))
+            if (!appointment)
+              throw new ExpectedError(
+                `Unable to find appointment with ID ${externalId} that existed at ${endOfBgoSnapshotDate}`
+              )
+            const tech = appointment.assignedTech
+            if (!tech)
+              throw new ExpectedError(
+                `The appointment with ID ${externalId} that existed at ${endOfBgoSnapshotDate} did not have an assigned tech`
+              )
+            return tech
+          })()
 
-      if (!tech) {
-        invalidRowsDetected.push({ reason: 'tech not found', row })
-        return
-      }
+          const sdcrWorkGroups = await (async () => {
+            const srWorkGroupTypes = ['Service Region', 'DMA', 'Office', 'Division']
+            const companyIds = _.filter([w2Company.id, subcontractor && subcontractor.id])
+            const getSrWorkGroups = async company => {
+              const workGroupExternalIds = srData[row['Service Region']]
+              return await WorkGroup.query()
+              .whereIn('companyId', companyIds)
+              .where(qb => {
+                srWorkGroupTypes.forEach(type => {
+                  qb.orWhere({ type, externalId: workGroupExternalIds[type] })
+                })
+              })
+            }
+            return _.flatten(
+              _.filter(
+                await Promise.all([
+                  tech.$relatedQuery('workGroups').whereNotIn('type', srWorkGroupTypes),
+                  getSrWorkGroups(w2Company),
+                  subcontractor && getSrWorkGroups(subcontractor),
+                ])
+              )
+            )
+          })()
 
-      const serviceRegionWorkGroups = ['Service Region', 'DMA', 'Office', 'Division']
-      const techGroups = await tech.$relatedQuery('workGroups').whereNotIn('type', serviceRegionWorkGroups)
-      let workOrderGroups = null
-      if (workOrder) {
-        workOrderGroups = await workOrder.$relatedQuery('workGroups').whereIn('type', serviceRegionWorkGroups)
-      } else {
-        const serviceRegionWorkGroupNames = await WorkGroup.knex()('directv_sr_data')
-        .where({
-          'Service Region': row['Service Region'],
-        })
-        .first()
-        const techCompanyWorkGroupIds = await tech
-        .$relatedQuery('workGroups')
-        .select('id')
-        .whereIn('type', ['Company', 'Subcontractor'])
-        const techCompanyIds = await WorkGroup.knex()('Company')
-        .select('id')
-        .whereIn('workGroupId', _.map(techCompanyWorkGroupIds, 'id'))
-        workOrderGroups = await WorkGroup.query()
-        .whereIn('companyId', _.map(techCompanyIds, 'id'))
-        .where(qb => {
-          qb
+          await SdcrDataPoint.query()
           .where({
-            type: 'Service Region',
-            name: serviceRegionWorkGroupNames['Service Region'],
+            externalId: row['Activity ID'],
+            date: row['BGO Snapshot Date'],
           })
-          .orWhere({
-            type: 'Office',
-            name: serviceRegionWorkGroupNames['Office'],
-          })
-          .orWhere({
-            type: 'DMA',
-            name: serviceRegionWorkGroupNames['DMA'],
-          })
-          .orWhere({
-            type: 'Division',
-            name: serviceRegionWorkGroupNames['Division'],
-          })
-        })
-      }
-      const sdcrWorkGroups = techGroups.concat(workOrderGroups)
-      await SdcrDataPoint.query()
-      .where({
-        workOrderExternalId: row['Activity ID'],
-        date: row['BGO Snapshot Date'],
-      })
-      .delete()
+          .delete()
 
-      const badProps = [
-        'HSP Partner Name',
-        'DMA',
-        'Office',
-        'Service Region',
-        'Tech Team',
-        'Tech ID',
-        'Tech Name',
-        'Subcontractor',
-        'Company Name',
-      ]
-      badProps.forEach(prop => {
-        delete row[prop]
-      })
-      row['Tech ID'] = tech.externalId
-      row['Tech Name'] = tech.name
-      const teamGroup = _.find(sdcrWorkGroups, { type: 'Team' })
-      row['Team Name'] = teamGroup && teamGroup.name
-      sdcrWorkGroups.forEach(workGroup => {
-        row[workGroup.type] = workGroup.externalId
-      })
+          const badProps = [
+            'HSP Partner Name',
+            'DMA',
+            'Office',
+            'Service Region',
+            'Tech Team',
+            'Tech ID',
+            'Tech Name',
+            'Subcontractor',
+            'Company Name',
+          ]
+          badProps.forEach(prop => {
+            delete row[prop]
+          })
+          row['Tech ID'] = tech.externalId
+          const teamGroup = _.find(sdcrWorkGroups, { type: 'Team' })
+          row['Team Name'] = teamGroup && teamGroup.name
+          sdcrWorkGroups.forEach(workGroup => {
+            row[workGroup.type] = workGroup.externalId
+          })
 
-      const sdcrDataPoint = await SdcrDataPoint.query().insert({
-        value: row['# of Same Day Activity Closed Count'] === '1' ? 1 : 0,
-        date: row['BGO Snapshot Date'],
-        workOrderId: workOrder && workOrder.id,
-        techId: tech.id,
-        workOrderExternalId: row['Activity ID'],
-        type: row['Activity Sub Type (Snapshot)'],
-        dwellingType: row['Dwelling Type'],
-        row: row,
-      })
-      await sdcrDataPoint.$relatedQuery('workGroups').relate(sdcrWorkGroups)
-    })
+          const sdcrDataPoint = await SdcrDataPoint.query().insert({
+            value: row['# of Same Day Activity Closed Count'] === '1' ? 1 : 0,
+            date: row['BGO Snapshot Date'],
+            techId: tech.id,
+            externalId: row['Activity ID'],
+            type: row['Activity Sub Type (Snapshot)'],
+            dwellingType: row['Dwelling Type'],
+            row: row,
+          })
+          await sdcrDataPoint.$relatedQuery('workGroups').relate(sdcrWorkGroups)
+        } catch (e) {
+          if (!(e instanceof ExpectedError)) throw e
+          invalidRowsDetected.push({
+            failureReason: e.message,
+            ...row,
+          })
+        }
+      },
+      { concurrency: 200 }
+    )
 
     if (invalidRowsDetected.length) {
       console.log('invalid row detected')
