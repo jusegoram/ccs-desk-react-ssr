ace82f259622cb7f1c19f751afbc1f120a95b826
Tim Huff <timhuff@gmail.com>
2018-05-31 17:35:44 -0500

    fix for raz

--- server/cli/commands/import/processors/closed/index.js
+++ server/cli/commands/import/processors/closed/index.js
@@ -123,18 +123,36 @@ export default async ({ csvObjStream, w2Company, now }) => {
             row[workGroup.type] = workGroup.externalId
           })
           const sdcrDataPointId = uuid()
-          const sdcrDataPoint = await SdcrDataPoint.query()
-          .insert({
-            id: sdcrDataPointId,
-            value: row['# of Same Day Activity Closed Count'] === '1' ? 1 : 0,
+          const existingSdcr = await SdcrDataPoint.query().findOne({
             date: row['BGO Snapshot Date'],
-            techId: tech ? tech.id : null,
             externalId: row['Activity ID'],
-            type: row['Activity Sub Type (Snapshot)'],
-            dwellingType: row['Dwelling Type'],
-            row: row,
           })
-          .returning('*')
+          let sdcrDataPoint = null
+          if (existingSdcr) {
+            sdcrDataPoint = await existingSdcr
+            .$query()
+            .patch({
+              value: row['# of Same Day Activity Closed Count'] === '1' ? 1 : 0,
+              techId: tech ? tech.id : null,
+              type: row['Activity Sub Type (Snapshot)'],
+              dwellingType: row['Dwelling Type'],
+              row: row,
+            })
+            .returning('*')
+          } else {
+            sdcrDataPoint = await SdcrDataPoint.query()
+            .insert({
+              id: sdcrDataPointId,
+              date: row['BGO Snapshot Date'],
+              externalId: row['Activity ID'],
+              value: row['# of Same Day Activity Closed Count'] === '1' ? 1 : 0,
+              techId: tech ? tech.id : null,
+              type: row['Activity Sub Type (Snapshot)'],
+              dwellingType: row['Dwelling Type'],
+              row: row,
+            })
+            .returning('*')
+          }
 
           workGroupSdcrDataPointsInserts.push(
             ...sdcrWorkGroups.map(workGroup => ({
