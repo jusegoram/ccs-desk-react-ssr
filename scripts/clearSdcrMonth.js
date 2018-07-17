cb4d55f4d349c9f7198ce9fe34bd07940d0d8151
Sebastian Gomez <jseb16@gmail.com>
2018-07-17 08:40:51 -0500

    delete it all

--- scripts/clearSdcrMonth.js
+++ scripts/clearSdcrMonth.js
@@ -7,25 +7,7 @@ const knex = Knex(knexfile['production'])
 moment.tz.setDefault('America/Chicago')
 
 const run = async () => {
-  const sdcrIds = knex('SdcrDataPoint')
-  .select('id')
-  .where(
-    'date',
-    '>=',
-    moment()
-    .startOf('month')
-    .format('YYYY-MM-DD')
-  )
-  .where(
-    'date',
-    '<=',
-    moment()
-    .endOf('month')
-    .format('YYYY-MM-DD')
-  )
-  await knex('workGroupSdcrDataPoints')
-  .whereIn('sdcrDataPointId', sdcrIds)
-  .delete()
-  await sdcrIds.delete()
+  await knex('workGroupSdcrDataPoints').delete()
+  await knex('SdcrDataPoint').delete()
 }
 run()
