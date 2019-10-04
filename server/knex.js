dfccfb615c69f1c86c95d74c9f3047134a4b6d66
Sebastian Gomez <jseb16@gmail.com>
2019-10-03 20:59:22 -0600

    bug fix

--- server/knex.js
+++ server/knex.js
@@ -3,6 +3,6 @@ import knexfile from 'server/../knexfile'
 
 export default Knex({
   debug: process.env.DEBUG,
-  //...knexfile[process.env.KNEX_ENV || process.env.NODE_ENV],
-  ...knexfile['development'],
+  ...knexfile[process.env.KNEX_ENV || process.env.NODE_ENV],
+  //...knexfile['development'],
 })
